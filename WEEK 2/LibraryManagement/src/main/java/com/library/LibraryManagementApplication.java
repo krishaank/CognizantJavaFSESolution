6900a1c9a9cd744ff5c93ea7a70109bc142b61c2
krishaank <krishank.dubey2424@gmail.com>
2026-07-02 19:57:53 +0530

    Update LibraryManagement application and configuration for IoC/DI

--- WEEK 2/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
+++ WEEK 2/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -4,16 +4,16 @@ import com.library.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Main {
+public class LibraryManagementApplication {
     public static void main(String[] args) {
-        // Load the Spring context from the XML configuration file
+        // 1. Initialize Spring IoC container from XML configuration
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // Retrieve the BookService bean
+        // 2. Retrieve the BookService bean (which already has BookRepository injected into it)
         BookService bookService = (BookService) context.getBean("bookService");
 
-        // Test the configuration by invoking the service method
-        System.out.println("Testing Spring IoC Configuration:");
+        // 3. Verify the dependency injection
+        System.out.println("Starting Library Management Application...");
         bookService.performServiceTask();
     }
 }
