6d6248970fa84f4349035bd94e650d3ff02c45ab
krishaank <krishank.dubey2424@gmail.com>
2026-07-02 20:04:50 +0530

    Add week 2 exercise 4 Maven configuration

---
+++ week 2 exercise 4/LibraryManagement/src/main/java/com/library/Main.java
@@ -0,0 +1,7 @@
+package com.library;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("LibraryManagement project initialized for Week 2 Exercise 4 with Java 1.8");
+    }
+}
